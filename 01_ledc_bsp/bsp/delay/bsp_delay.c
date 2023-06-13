6044d7d11033434df3ae88ce28f0ad9400d1f469
LQF376 <96860683+LQF376@users.noreply.github.com>
2023-06-13 17:05:33 +0800

    Create bsp_delay.c

---
+++ 01_ledc_bsp/bsp/delay/bsp_delay.c
@@ -0,0 +1,27 @@
+#include "bsp_delay.h"
+
+/*
+ * @description	: 短时间延时函数
+ * @param - n	: 要延时循环次数(空操作循环次数，模式延时)
+ * @return 		: 无
+ */
+void delay_short(volatile unsigned int n)
+{
+	while(n--){}
+}
+
+/*
+ * @description	: 延时函数,在396Mhz的主频下
+ * 			  	  延时时间大约为1ms
+ * @param - n	: 要延时的ms数
+ * @return 		: 无
+ */
+void delay(volatile unsigned int n)
+{
+	while(n--)
+	{
+		delay_short(0x7ff);
+	}
+}
+
+
