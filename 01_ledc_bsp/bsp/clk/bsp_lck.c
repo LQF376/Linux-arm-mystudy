3cf7b97eb681126e51124fa303aac80ef6efff32
LQF376 <96860683+LQF376@users.noreply.github.com>
2023-06-13 17:03:40 +0800

    Create bsp_lck.c

---
+++ 01_ledc_bsp/bsp/clk/bsp_lck.c
@@ -0,0 +1,18 @@
+#include "bsp_clk.h"
+
+/*
+ * @description	: 使能I.MX6U所有外设时钟
+ * @param 		: 无
+ * @return 		: 无
+ */
+void clk_enable(void)
+{
+	CCM->CCGR0 = 0XFFFFFFFF;
+	CCM->CCGR1 = 0XFFFFFFFF;
+	CCM->CCGR2 = 0XFFFFFFFF;
+	CCM->CCGR3 = 0XFFFFFFFF;
+	CCM->CCGR4 = 0XFFFFFFFF;
+	CCM->CCGR5 = 0XFFFFFFFF;
+	CCM->CCGR6 = 0XFFFFFFFF;
+}
+
