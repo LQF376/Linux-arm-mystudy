4275c1ec494e6a2961d0b29ace42d0cb43cba4c6
LQF376 <96860683+LQF376@users.noreply.github.com>
2023-06-15 16:21:20 +0800

    Update bsp_gpio.h

--- 02_clk/bsp/gpio/bsp_gpio.h
+++ 02_clk/bsp/gpio/bsp_gpio.h
@@ -2,16 +2,8 @@
 #define _BSP_GPIO_H
 #define _BSP_KEY_H
 #include "imx6ul.h"
-/***************************************************************
-Copyright © zuozhongkai Co., Ltd. 1998-2019. All rights reserved.
-文件名	: 	 bsp_gpio.h
-作者	   : 左忠凯
-版本	   : V1.0
-描述	   : GPIO操作文件头文件。
-其他	   : 无
-论坛 	   : www.wtmembed.com
-日志	   : 初版V1.0 2019/1/4 左忠凯创建
-***************************************************************/
+
+/*  GPIO操作文件头文件 */
 
 /* 枚举类型和结构体定义 */
 typedef enum _gpio_pin_direction
