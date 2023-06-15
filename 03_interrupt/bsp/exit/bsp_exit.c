4c3d6f371cbc2a49a4c2595237c78aa7e29ebdcb
LQF376 <96860683+LQF376@users.noreply.github.com>
2023-06-15 21:56:03 +0800

    Update bsp_exit.c

--- 03_interrupt/bsp/exit/bsp_exit.c
+++ 03_interrupt/bsp/exit/bsp_exit.c
@@ -1,13 +1,5 @@
-/***************************************************************
-Copyright © zuozhongkai Co., Ltd. 1998-2019. All rights reserved.
-文件名	: 	 bsp_exit.c
-作者	   : 左忠凯
-版本	   : V1.0
-描述	   : 外部中断驱动。
-其他	   : 配置按键对应的GPIP为中断模式
-论坛 	   : www.wtmembed.com
-日志	   : 初版V1.0 2019/1/4 左忠凯创建
-***************************************************************/
+/* 配置按键对应的GPIP为中断模式 */
+
 #include "bsp_exit.h"
 #include "bsp_gpio.h"
 #include "bsp_int.h"
