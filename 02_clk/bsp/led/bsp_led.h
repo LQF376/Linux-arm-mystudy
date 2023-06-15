56cb2b018b4117c010619b75d13335b36fdd37e8
LQF376 <96860683+LQF376@users.noreply.github.com>
2023-06-15 16:24:22 +0800

    Update bsp_led.h

--- 02_clk/bsp/led/bsp_led.h
+++ 02_clk/bsp/led/bsp_led.h
@@ -1,16 +1,8 @@
 #ifndef __BSP_LED_H
 #define __BSP_LED_H
 #include "imx6ul.h"
-/***************************************************************
-Copyright © zuozhongkai Co., Ltd. 1998-2019. All rights reserved.
-文件名	: 	 bsp_led.h
-作者	   : 左忠凯
-版本	   : V1.0
-描述	   : LED驱动头文件。
-其他	   : 无
-论坛 	   : www.wtmembed.com
-日志	   : 初版V1.0 2019/1/4 左忠凯创建
-***************************************************************/
+
+/*  LED驱动头文件 */
 
 #define LED0	0
 
