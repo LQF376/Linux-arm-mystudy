6fed056b92cc78ad2b89dafd6d177db48e7775cf
LQF376 <96860683+LQF376@users.noreply.github.com>
2023-06-15 22:02:19 +0800

    Update bsp_key.c

--- 03_interrupt/bsp/key/bsp_key.c
+++ 03_interrupt/bsp/key/bsp_key.c
@@ -1,16 +1,10 @@
 #include "bsp_key.h"
 #include "bsp_gpio.h"
 #include "bsp_delay.h"
-/***************************************************************
-Copyright © zuozhongkai Co., Ltd. 1998-2019. All rights reserved.
-文件名	: 	 bsp_key.h
-作者	   : 左忠凯
-版本	   : V1.0
-描述	   : 按键驱动文件。
-其他	   : 无
-论坛 	   : www.wtmembed.com
-日志	   : 初版V1.0 2019/1/4 左忠凯创建
-***************************************************************/
+
+/*
+按键驱动文件
+*/
 
 /*
  * @description	: 初始化按键
