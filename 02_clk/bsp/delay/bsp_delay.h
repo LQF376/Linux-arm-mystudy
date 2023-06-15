ae1e0c4780d8633fb87120f2cf894340d1ff5caf
LQF376 <96860683+LQF376@users.noreply.github.com>
2023-06-15 16:19:23 +0800

    Update bsp_delay.h

--- 02_clk/bsp/delay/bsp_delay.h
+++ 02_clk/bsp/delay/bsp_delay.h
@@ -1,15 +1,8 @@
 #ifndef __BSP_DELAY_H
 #define __BSP_DELAY_H
-/***************************************************************
-Copyright © zuozhongkai Co., Ltd. 1998-2019. All rights reserved.
-文件名	: 	 bsp_delay.h
-作者	   : 左忠凯
-版本	   : V1.0
-描述	   : 延时头文件。
-其他	   : 无
-论坛 	   : www.wtmembed.com
-日志	   : 初版V1.0 2019/1/4 左忠凯创建
-***************************************************************/
+
+/* 延时头文件 */
+
 #include "imx6ul.h"
 
 
